d042c7dc4174c2b82493f5e4a129ef08d0680dc2
Maria Naveen <marianaveen15@outlook.com>
2024-09-06 07:40:42 +0530

    Updated the store to include the reducers of search Slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slices/userSlice";
+import searchReducer from "../slices/searchSlice";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    search: searchReducer,
   },
 });
